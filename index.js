0a0d2cd5a924e472a24df7f404459fb7268176e2
Alexandre Morin <amorin@adobe.com>
2020-09-27 21:00:39 +0200

    Fix stupid error

--- index.js
+++ index.js
@@ -122,8 +122,8 @@ async function createTag(dir, config, version) {
 }
 
 async function publishPackage(dir, config, version) {
-    console.log(`GITHUB_TOKEN: ${getEnv(GITHUB_TOKEN)}`);
-    console.log(`NPM_AUTH_TOKEN: ${getEnv(NPM_AUTH_TOKEN)}`);
+    console.log(`GITHUB_TOKEN: ${getEnv("GITHUB_TOKEN")}`);
+    console.log(`NPM_AUTH_TOKEN: ${getEnv("NPM_AUTH_TOKEN")}`);
   await run(
     dir,
     "yarn",
